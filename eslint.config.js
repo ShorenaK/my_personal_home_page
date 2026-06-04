92c68a39bd4b5d9a9ad5fbf0316d77284ec1c074
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-06-04 19:03:20 -0400

    package json added

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// ESLint Configuration File
+
+export default [
+  {
+    files: ['js/**/*.js'],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+];
